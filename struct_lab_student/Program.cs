351be7f9eacf303c09209361145b2cce215ea8c2
Vova Semenchenko <semenchenko133@gmail.com>
2019-04-25 08:45:34 +0300

    First commit

---
+++ struct_lab_student/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
